12ef33b90d0c83f3262a3b0a24490bcbf8b8b5bb
Micha Bernhard <mib70@pitt.edu>
2022-01-05 17:10:42 -0700

    Change timer validations

--- app/models/timer.rb
+++ app/models/timer.rb
@@ -3,6 +3,4 @@ class Timer < ApplicationRecord
 
   validates_presence_of :name
   validates_presence_of :duration
-  validates_presence_of :substance
-  validates_presence_of :dosage
 end
--- spec/models/timer_spec.rb
+++ spec/models/timer_spec.rb
@@ -8,7 +8,5 @@ RSpec.describe Timer do
   describe 'validations' do
     it { validate_presence_of :name }
     it { validate_presence_of :duration }
-    it { validate_presence_of :substance }
-    it { validate_presence_of :dosage }
   end
 end
