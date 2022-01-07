57c3db439a39e89c42d425f7543c322a4039bcad
Micha Bernhard <mib70@pitt.edu>
2022-01-06 20:07:48 -0700

    Test: Write test for timer delete endpoint

--- spec/requests/api/v1/timers_request_spec.rb
+++ spec/requests/api/v1/timers_request_spec.rb
@@ -95,17 +95,70 @@ RSpec.describe 'Timers API' do
   describe 'PATCH /api/v1/timers/:id' do
     let!(:timer1) { create(:timer, user_id: 1, id: 1) }
     
-    item_params = { name: "New Name" }
+    timer_params = { name: "New Name" }
     headers = {"CONTENT_TYPE" => "application/json"}
 
 
-    before { patch '/api/v1/timers/1', headers: headers, params: JSON.generate(item_params) }
+    before { patch '/api/v1/timers/1', headers: headers, params: JSON.generate(timer: timer_params) }
     
     it 'should update the timer' do
       expect(response).to be_successful
 
       timer1.reload
       expect(timer1.name).to eq 'New Name'
+    end
+  end
+
+  describe 'POST /api/v1/timers' do
+    it 'should create a new Timer if the params are correct' do
+      timer_params = { 
+        user_id: 1,
+        name: 'Timer',
+        duration: 120,
+        substance: 'Drug',
+        dosage: '10oz',
+        entry_instructions: 'The building code is 1234',
+        notes: 'These are some notes'
+      }
+  
+      headers = {"CONTENT_TYPE" => "application/json"}
+
+      post '/api/v1/timers', headers: headers, params: JSON.generate(timer: timer_params)
+
+      timer = Timer.last
+
+      expect(timer.user_id).to eq 1
+      expect(timer.name).to eq 'Timer'
+      expect(timer.duration).to eq 120
+      expect(timer.substance).to eq 'Drug'
+      expect(timer.dosage).to eq '10oz'
+      expect(timer.entry_instructions).to eq 'The building code is 1234'
+      expect(timer.notes).to eq 'These are some notes'
+    end
+
+    # it 'should not create a timer if the params are not valid' do
+    #   timer_params = { 
+    #     user_id: 1,
+    #     name: 'Timer',
+    #   }
+  
+    #   headers = {"CONTENT_TYPE" => "application/json"}
+
+    #   post '/api/v1/timers', headers: headers, params: JSON.generate(timer: timer_params)
+
+    #   expect(response.status).to be_unsuccessful
+    #   expect(response.body).to eq 'Timer could not be created'
+    # end
+  end
+
+  describe 'DELETE /api/v1/timers/:id' do
+    let!(:timer) { create(:timer, id: 1) }
+
+    it 'should delete the specified timer' do
+      delete '/api/v1/timers/1'
+
+      expect(response).to be_successful
+      expect(Timer.all.count).to eq 0
     end
   end
 end 
