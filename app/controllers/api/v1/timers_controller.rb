f41cf73d0d4f5c9de89e10a219b56f46cd9715b1
Micha Bernhard <mib70@pitt.edu>
2022-01-06 18:35:12 -0700

    Feat: Write timer show method

--- app/controllers/api/v1/timers_controller.rb
+++ app/controllers/api/v1/timers_controller.rb
@@ -2,4 +2,8 @@ class Api::V1::TimersController < ApplicationController
   def index
     render json: TimerSerializer.new(Timer.where(user_id: params[:user_id]))
   end
+
+  def show
+    render json: TimerSerializer.new(Timer.find(params[:id]))
+  end
 end
