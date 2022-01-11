ca32be6e83bcbdb6ecc631492cc5acee45c0a315
Jacob Yarborough <jayarborough95@gmail.com>
2022-01-11 15:28:29 -0700

    Update redis server url

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,7 +1,9 @@
 Sidekiq.configure_server do |config|
-  config.redis = { url: 'redis://localhost:6379/0' }
+  config.redis = { url: 'redis://redistogo:b43f8de26bfabaacb4f6e0ccb1fb6e60@crestfish.redistogo.com:11680/' }
 end
 
 Sidekiq.configure_client do |config|
-  config.redis = { url: 'redis://localhost:6379/0' }
+  config.redis = { url: 'redis://redistogo:b43f8de26bfabaacb4f6e0ccb1fb6e60@crestfish.redistogo.com:11680/' }
 end
+
+# redis://localhost:6379/0
