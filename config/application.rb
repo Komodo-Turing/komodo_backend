82d87629118a548b3f2175c15bc9cd9cb679e56a
Jacob Yarborough <jayarborough95@gmail.com>
2022-01-12 09:49:58 -0700

    Update config

--- config/application.rb
+++ config/application.rb
@@ -32,6 +32,7 @@ module KomodoBackend
     config.session_store :cookie_store, key: '_interslice_session'
     config.middleware.use ActionDispatch::Cookies
     config.middleware.use config.session_store, config.session_options
+    config.active_job.queue_adapter = :sidekiq
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
   end
