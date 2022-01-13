899ac6567971be7b6a9d882bebf5d6a9e3830470
Jacob Yarborough <jayarborough95@gmail.com>
2022-01-13 15:51:15 -0700

    Refactor create_body method

--- app/models/active_timer.rb
+++ app/models/active_timer.rb
@@ -1,5 +1,5 @@
 class ActiveTimer < ApplicationRecord
   def create_body(substance, dosage, entry_instructions, notes, address, image_url)
-    'You are recieving this text, because (insert name here) did not respond to a timer and may be experiencing an overdose. \n' + 'Substance: ' + substance + '\n' + 'Dosage: ' + dosage + '\n' + Address: ' + address' + '\n' + 'Entry Instructions: ' + entry_instructions + '\n' + 'Notes: ' + notes 
+    'You are recieving this text, because (insert name here) did not respond to a timer and may be experiencing an overdose. \n' + 'Substance: ' + substance + '\n' + 'Dosage: ' + dosage + '\n' + 'Address: ' + address + '\n' + 'Entry Instructions: ' + entry_instructions + '\n' + 'Notes: ' + notes 
   end 
 end
